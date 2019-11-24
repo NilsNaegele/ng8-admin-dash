ddd4433e0735a2d2181908d420bb1c54f4b4fd53
NilsNaegele <nils.holger.naegele@gmail.com>
2019-11-24 10:44:29 +0100

    forgot-password cmp

--- src/app/pages/user/forgot-password/forgot-password.component.ts
+++ src/app/pages/user/forgot-password/forgot-password.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BlankLayoutCardComponent } from '../../../components/blank-layout-card/blank-layout-card.component';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss'],
+  styleUrls: ['../../../components/blank-layout-card/blank-layout-card.component.scss'],
 })
-export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ForgotPasswordComponent extends BlankLayoutCardComponent {
 }

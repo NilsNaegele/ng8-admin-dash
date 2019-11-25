93ddbdd27f121857a6cf386998aa486be3cba07a
NilsNaegele <nils.holger.naegele@gmail.com>
2019-11-25 12:12:04 +0100

    dodecahedroncard cmp

--- src/app/pages/dashboard/dodecahedron-card/dodecahedron-card.component.ts
+++ src/app/pages/dashboard/dodecahedron-card/dodecahedron-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-dodecahedron-card',
   templateUrl: './dodecahedron-card.component.html',
   styleUrls: ['./dodecahedron-card.component.scss'],
 })
-export class DodecahedronCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class DodecahedronCardComponent {
+  @HostBinding('class.dodecahedron') public readonly dodecahedron = true;
 
 }

26a44d607bea5516197fb7116ca1d51bb6b63609
NilsNaegele <nils.holger.naegele@gmail.com>
2019-12-01 06:54:46 +0100

    sliders cmp

--- src/app/pages/components/components.module.ts
+++ src/app/pages/components/components.module.ts
@@ -8,9 +8,16 @@ import { BadgesComponent } from './badges/badges.component';
 import { ChipsComponent } from './chips/chips.component';
 import { ComponentsComponent } from './components.component';
 import { ProgressBarsComponent } from './progress-bars/progress-bars.component';
+import { SlidersComponent } from './sliders/sliders.component';
 
 @NgModule({
-  declarations: [ProgressBarsComponent, ComponentsComponent, BadgesComponent, ChipsComponent],
+  declarations: [
+    ProgressBarsComponent,
+    ComponentsComponent,
+    BadgesComponent,
+    ChipsComponent,
+    SlidersComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
---
+++ src/app/pages/components/sliders/sliders.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sliders',
+  templateUrl: './sliders.component.html',
+  styleUrls: ['./sliders.component.scss'],
+})
+export class SlidersComponent { }
